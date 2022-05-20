9e6c6919cbf85988f45a2056b8d9f58a43088c9a
curlypelican <90104752+curlypelican@users.noreply.github.com>
2022-05-20 21:05:02 +0300

    Add files via upload

---
+++ src/main/java/ru/mirea/budget/controller/MonetaryOperationController.java
@@ -0,0 +1,39 @@
+package ru.mirea.budget.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.mirea.budget.dto.AccountBudgetInfoDto;
+import ru.mirea.budget.entity.MonetaryOperation;
+import ru.mirea.budget.service.MonetaryOperationService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequestMapping("/monetary")
+@RequiredArgsConstructor
+@RestController
+public class MonetaryOperationController {
+    private final MonetaryOperationService operationService;
+
+    @GetMapping("/user/{userId}")
+    public List<MonetaryOperation> getAllByUserId(@PathVariable("userId") String userId) {
+        return operationService.getAllByUserId(userId);
+    }
+
+    @GetMapping("/budget/total/user/{userId}")
+    public AccountBudgetInfoDto getBudgetInfoByUserId(@PathVariable("userId") String userId) {
+        return operationService.getBudgetInfoByUserId(userId);
+    }
+
+    @PostMapping(value = "")
+    public MonetaryOperation createMonetaryOperation(@RequestBody @Valid MonetaryOperation monetaryOperation) {
+        return operationService.createMonetaryOp(monetaryOperation);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteMonetaryOperationById(@PathVariable("id") String id) {
+        operationService.deleteById(id);
+    }
+
+}
